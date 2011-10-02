c9b713dd5d775be4204f0aa2b2b5a44a79960267
Amit Levy <aalevy@gmail.com>
2011-10-02 14:55:27 -0700

    hlint errors fixed and ignore dist folder

--- hist.hs
+++ hist.hs
@@ -20,22 +20,24 @@ countWords ws = foldr addOccurance Map.empty ws where
 
 histogram :: Int -> Int -> (String, Int) -> String
 histogram max_key_length max_count (key, count) = line_from_key_count where
-  line_from_key_count = key ++ (nchars " " ' ' (max_key_length - length(key))) ++ (nchars "" '#' ((80 - max_key_length - 1) * count `div` max_count))
+  line_from_key_count = key ++ nchars " " ' ' (max_key_length - length key) ++
+                          nchars "" '#' ((80 - max_key_length - 1 *
+                                            count `div` max_count))
   nchars str c n | n <= 0 = str
                  | otherwise = nchars (c:str) c (n - 1)
 
 toPairs :: Map.Map String Int -> [(String, Int)]
-toPairs counts = Map.toList(counts)
+toPairs counts = Map.toList counts
 
 compareCounts :: (String, Int) -> (String, Int) -> Ordering
 compareCounts (_, count1) (_, count2) = compare count1 count2
 
 main = do (file_name:_) <- getArgs
           text <- readFile file_name
-          let ws = (map normalize (words text))
+          let ws = map normalize (words text)
           let counts = countWords ws
-          let max_key_length = (maximum (map length (Map.keys counts)))
-          let max_count = (maximum (Map.elems counts))
+          let max_key_length = maximum (map length (Map.keys counts))
+          let max_count = maximum (Map.elems counts)
           let pairs =  reverse $ sortBy compareCounts $ toPairs counts
-          let hist = (map (histogram max_key_length max_count) pairs)
+          let hist = map (histogram max_key_length max_count) pairs
           sequence (map putStrLn hist)
