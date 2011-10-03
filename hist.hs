a48ccf362d4653425032f654f5b17c29b1e63aa1
Amit Levy <aalevy@gmail.com>
2011-10-03 01:05:17 -0700

    Cleanup code

--- hist.hs
+++ hist.hs
@@ -13,32 +13,29 @@ normalize word = map toLower (strip_cruft word) where
 isWord :: String -> Bool
 isWord str = any isAlpha str
 
-addOccurance :: Ord a => a -> Map.Map a Int -> Map.Map a Int
-addOccurance word m = Map.insert word (count + 1) m where
-  count | Map.member word m = m Map.! word
-        | otherwise = 0
-
 countWords :: Ord a => [a] -> Map.Map a Int
 countWords ws = foldr addOccurance Map.empty ws where
+  addOccurance word m = Map.insert word (count + 1) m where
+    count | Map.member word m = m Map.! word
+          | otherwise = 0
 
-histogram :: Int -> Int -> (String, Int) -> IO ()
-histogram max_key_length max_count (key, count) = putStrLn line_from_key_count where
-  line_from_key_count = key ++ nchars " " ' ' (max_key_length - length key) ++
+histogramLine :: Int -> Int -> (String, Int) -> String
+histogramLine max_key_length max_count (key, count) = key ++ nchars " " ' ' (max_key_length - length key) ++
                           nchars "" '#' ((80 - max_key_length - 1) *
-                                            count `div` max_count)
-  nchars str c n | n <= 0 = str
+                                            count `div` max_count) where
+  nchars str c n | n <= 0 = str -- Append n of c to str
                  | otherwise = nchars (c:str) c (n - 1)
 
-compareCounts :: (String, Int) -> (String, Int) -> Ordering
-compareCounts (_, count1) (_, count2) = compare count1 count2
+histogramPairs text = reverse $ sortBy compareCounts $ Map.toList $ countWords ws where
+                        ws = filter isWord $ map normalize (words text)
+                        compareCounts (_, count1) (_, count2) = compare count1 count2
 
 main = do args <- getArgs
-          let contents | length args > 0 = readFile $ head args
-                       | otherwise = getContents
-          text <- contents
-          let ws = filter isWord $ map normalize (words text)
-          let counts = countWords ws
-          let max_key_length = maximum (map length (Map.keys counts))
-          let max_count = maximum (Map.elems counts)
-          let pairs =  reverse $ sortBy compareCounts $ Map.toList counts
-          mapM (histogram max_key_length max_count) pairs
+          text <- if length args > 0 then readFile $ head args
+                 else getContents
+          -- Process pairs into [(word, count)]
+          let pairs = histogramPairs text
+          let max_key_length = maximum $ map (length . fst) pairs
+          let max_count = maximum $ map snd pairs
+          -- And output results
+          mapM (putStrLn . histogramLine max_key_length max_count) pairs
